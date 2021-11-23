719291eb47efc6cb2133cbdf0e660d4e24538e90
Brad Russell <brad.p.russell@gmail.com>
2021-11-23 18:08:50 -0500

    Testing with 10 sessions

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # VstheWorld
-This is a solution to help with the "The Pole Mine: Vs the World" achievement in The Jackbox Party Pack 8
+This is a solution to help with the "The Pole Mine: Vs the World" achievement in The Jackbox Party Pack 8.
+
+This achievement requires the player to win a game of The Pole Mine in streamer mode with an audience of at least 1000. This application opens 1000 headless Chrome browser instances and logs into the game corresponding to the room code provided when running the script.
 
 # Installation
 Install the latest version of NodeJS at https://nodejs.org/
 
-Open Powershell and use Node Package Manager to install Puppeteer
+Open Powershell, navigate to the directory where the repo was cloned, and use Node Package Manager to install Puppeteer.
     
     npm i puppeteer --save
--- VstheWorld.js
+++ VstheWorld.js
@@ -4,23 +4,30 @@ const url = 'https://jackbox.tv';
 if (!roomCode) {
     throw "Please provide room code as a first argument";
 }
-async function run () {
+async function run (sessionId) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
+    // enter room code
     const roomCodeElements = await page.$x("//input[@id='roomcode']")
     await roomCodeElements[0].type(roomCode);
 
+    // enter user name
     const usernameElements = await page.$x("//input[@id='username']")
-    await usernameElements[0].type("foo");
+    await usernameElements[0].type("User" + sessionId);
 
+    // click "Join Audience" button once available
     await page.waitForSelector(".audience");
-    //const joinButtonElements = await page.$x("//input[@class='audience']")
     await page.click("#button-join");
     
+    // When game ends, browser displays "DISCONNECTED", so we are done
     await page.waitForXPath('//*[contains(text(), "DISCONNECTED")]');
 
     browser.close();
 }
-run();
+async function runAll(){
+    const sessions = [...Array(10).keys()].map(x => run(x)); // set up 1000 sessions
+    await Promise.all(sessions);
+}
+runAll();
