2f6de1d2997a859b3a2108904ebdc5c24e34e053
Brad Russell <brad.p.russell@gmail.com>
2021-11-23 23:09:50 -0500

    Running with 100 listeners

--- VstheWorld.js
+++ VstheWorld.js
@@ -1,33 +1,46 @@
 const puppeteer = require('puppeteer');
 const roomCode = process.argv[2];
 const url = 'https://jackbox.tv';
+process.setMaxListeners(Infinity);
 if (!roomCode) {
     throw "Please provide room code as a first argument";
 }
 async function run (sessionId) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    //turns request interceptor on
+    await page.setRequestInterception(true);
+
+    //if the page makes a  request to a resource type of image or stylesheet then abort that request
+    page.on('request', request => {
+    if (request.resourceType() === 'image')
+        request.abort();
+    else
+        request.continue();
+    });
+
+    // go to jackbox.tv
     await page.goto(url);
 
     // enter room code
-    const roomCodeElements = await page.$x("//input[@id='roomcode']")
-    await roomCodeElements[0].type(roomCode);
+    await page.waitForSelector("#roomcode",{timeout:500000});
+    await page.type("#roomcode", roomCode);
 
     // enter user name
-    const usernameElements = await page.$x("//input[@id='username']")
-    await usernameElements[0].type("User" + sessionId);
+    await page.waitForSelector("#username",{timeout:500000});
+    await page.type("#username", "User" + sessionId);
 
     // click "Join Audience" button once available
-    await page.waitForSelector(".audience");
+    await page.waitForSelector(".audience",{timeout:500000});
     await page.click("#button-join");
     
     // When game ends, browser displays "DISCONNECTED", so we are done
-    await page.waitForXPath('//*[contains(text(), "DISCONNECTED")]');
+    await page.waitForXPath('//*[contains(text(), "DISCONNECTED")]',{timeout:500000});
 
     browser.close();
 }
 async function runAll(){
-    const sessions = [...Array(10).keys()].map(x => run(x)); // set up 1000 sessions
+    const sessions = [...Array(100).keys()].map(x => run(x)); // set up 1000 sessions
     await Promise.all(sessions);
 }
 runAll();
